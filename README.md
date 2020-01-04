edeba0820bbd442dfbdb48812bdb35bd74357d4b
preethamrn <preetham.in.1997@gmail.com>
2020-01-03 23:33:58 -0800

    Implemented basic drag and drop and scaling functionality

--- README.md
+++ README.md
@@ -32,14 +32,13 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 # TODO
 - Setup all den Pokemon (update denPokemon.py script to generate the required component data)
 - Setup all den positions
+- Fix the transform-origin for scaling (it doesn't feel like the natural center of the screen is being preserved)
 - Fix styling for the hover layout
 - Add deep links into Serebii
 - Add contact and credits links
 - Search for dens by Pokemon
 - Add other map elements (trees, items, nursery, watts trader, digging duo) + toggle map elements functionality
-- Draggable map (handleDrag can also update all the den location positions): https://vuejsexamples.com/a-vue-component-that-create-moveable-and-resizable/
-- Resize buttons on map (change the scale)
-- Scale map to browser width on page load (only once!)
+- Scale map and center the transform to browser width on page load (only once!)
 
 # DONE
 - Map with den markers positioning functionality
@@ -47,3 +46,5 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 - Show Pokemon icons (download sprites from Serebii)
 - Add list of all pokemon to each den (from Serebii)
 - Support list of Pokemon in a table
+- Draggable map (handleDrag can also update all the den location positions): https://vuejsexamples.com/a-vue-component-that-create-moveable-and-resizable/
+- Resize buttons on map (change the scale)
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div id="app">
-    <div id='wild-area-map'>
-      <img alt="Pokemon Wild Area Map" src="./assets/pokemon-wild-area.jpg">
-      <DenLocation v-for="(den, index) in dens" :key="index" :position='den.position' :commonDen='denPokemon[den.commonID]' :rareDen='denPokemon[den.rareID]' :gmax='denPokemon[den.commonID].gmax || denPokemon[den.rareID].gmax' :screenshotImg='den.img'/>
+    <v-btn id='resetBtn' @click='reset'>Recenter</v-btn>
+    <Moveable v-bind='moveable' @drag='handleDrag' ref='moveableTarget'>
+      <div id='wild-area-map' ref='wildAreaMap'>
+        <img alt="Pokemon Wild Area Map" src="./assets/pokemon-wild-area.jpg">
+        <DenLocation v-for="(den, index) in dens" :key="index" :position='den.position' :commonDen='denPokemon[den.commonID]' :rareDen='denPokemon[den.rareID]' :gmax='denPokemon[den.commonID].gmax || denPokemon[den.rareID].gmax' :screenshotImg='den.img'/>
+      </div>
+    </Moveable>
+    <div id='scalingControls'>
+      <v-btn @click='scaleDown'>-</v-btn>
+      <v-btn @click='scaleUp'>+</v-btn>
     </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable no-param-reassign,no-unused-expressions,no-unused-vars,no-console */
+import Moveable from 'vue-moveable'
 import DenLocation from './components/DenLocation.vue'
 
 export default {
   name: 'app',
   components: {
-    DenLocation
+    DenLocation,
+    Moveable,
   },
   data() {
     return {
+      // support dragging map
+      moveable: {
+        draggable: true,
+      },
+      currentScale: 1.0,
+
+      // den data (immutable)
       dens: [
         {commonID: 18, rareID: 77, location: "Lake of Outrage", img: "https://www.serebii.net/swordshield/den/th/lakeofoutrage-3.jpg",  position: {x: 362, y: 215}},
         {commonID: 38, rareID: 64, location: "Axew's Eye", img: "https://www.serebii.net/swordshield/den/th/axew'seye-1.jpg",  position: {x: 572, y: 1456}},
@@ -28,6 +45,30 @@ export default {
         77: {name: 'Den 77', link: "https://www.serebii.net/swordshield/maxraidbattles/den77.shtml", gmax: true, swordPokemon: ['037', '850', '607', '004', '005', '038', '631', '324', '038', '758', '851', '006-gi'], shieldPokemon: ['058', '850', '607', '004', '005', '631', '631', '324', '758', '059', '851', '006-gi']},
       },
     }
+  },
+  methods: {
+    handleDrag({target, transform}) {
+      target.style.transform = transform
+    },
+    reset() {
+      this.currentScale = 1.0
+      this.$refs.moveableTarget.$el.style.transform = `translate(0px, 0px) translateZ(50px)`
+      this.$refs.wildAreaMap.style.transform = `scale(${this.currentScale})`
+    },
+    scaleDown() {
+      // minScale = 0.5
+      if (this.currentScale > 0.5) {
+        this.currentScale -= 0.5
+        this.$refs.wildAreaMap.style.transform = `scale(${this.currentScale})`
+      }
+    },
+    scaleUp() {
+      // maxScale = 3.0
+      if (this.currentScale < 3.0) {
+        this.currentScale += 0.5
+        this.$refs.wildAreaMap.style.transform = `scale(${this.currentScale})`
+      }
+    },
   }
 }
 </script>
@@ -41,10 +82,25 @@ export default {
   background-color: #b1cf4b;
   padding-bottom: 500px;
 }
+#resetBtn {
+  position: fixed;
+  top: 20px; left: 90vw;
+  z-index: 100;
+}
+#scalingControls {
+  position: fixed;
+  top: 90vh; left: 90vw;
+  z-index: 100;
+}
 
 #wild-area-map {
   position: relative;
+  transform-origin: top left;
   top: 15px;
   left: 10px;
+}
+
+.moveable-control,.moveable-line {
+  display: none;
 }
 </style>
