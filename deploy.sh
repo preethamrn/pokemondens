b489a32bfa8780e955caad1f3749ab8266858002
Preetham Narayanareddy <preethamrn@uber.com>
2020-01-19 19:20:36 -0800

    Added deployment script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+echo 'www.pokemondenmap.com' > CNAME
+
+git init
+git add -A
+git commit -m "deploy $1" 
+git config --local credential.helper ""
+
+git push -f https://github.com/pokemondenmap/pokemondenmap.github.io master
+
+cd -
